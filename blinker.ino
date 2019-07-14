715375a5f5718771485724567720bae0969187df
Matthias Müller <52663038+CeresMathew@users.noreply.github.com>
2019-07-14 16:11:09 +0200

    Add files via upload
    Blinker funktioniert mit 2 schalter jedoch bleiben die neos noch on

--- blinker.ino
+++ blinker.ino
@@ -9,6 +9,12 @@
 
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+const int buttonrechts = 2;     // the number of the pushbutton pin
+const int buttonlinks = 3;     // the number of the pushbutton pin
+
+int buttonStateRe = 0;  
+int buttonStateLi = 0;  
+
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 8 // Popular NeoPixel ring size
@@ -20,51 +26,56 @@
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 100 // Time (in milliseconds) to pause between pixels
- 
-  
-// constants won't change. They're used here to set pin numbers:
-const int BlinkerRechts = 2;     // the number of the pushbutton pin
-const int BlinkerLinks = 4;     // the number of the pushbutton pin
-
 
 void setup() {
- pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-
-
-// initialize the pushbutton pin as an input:
-  pinMode(BlinkerRechts, INPUT);
-  pinMode(BlinkerLinks, INPUT);
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+    // initialize the pushbutton pin as an input:
+  pinMode(buttonrechts, INPUT);
+  pinMode(buttonlinks, INPUT);
 }
 
 void loop() {
   pixels.clear(); // Set all pixel colors to 'off'
 
   // read the state of the pushbutton value:
-  buttonStateBR = digitalRead(BlinkerRechts);
-  buttonStateBL = digitalRead(BlinkerLinks);
-
-   if (buttonStateBR == HIGH) {
-  for(int r=4; r<NUMPIXELS; r++) { // For each pixel...
-
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(r, pixels.Color(255, 60, 0));
-
-    pixels.show();   // Send the updated pixel colors to the hardware.
-
-    delay(DELAYVAL); // Pause before next pass through loop
-  }
-   if (buttonStateBL == HIGH) {
-  for(int l=3; l<=0; l--) { // For each pixel...
+  buttonStateRe = digitalRead(buttonrechts);
+  buttonStateLi = digitalRead(buttonlinks);
+  
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonStateRe == HIGH) {
+    // turn LED on:
+  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
-    pixels.setPixelColor(l, pixels.Color(255, 60, 0));
-
+    pixels.setPixelColor(i, pixels.Color(255, 80, 0));
     pixels.show();   // Send the updated pixel colors to the hardware.
-
     delay(DELAYVAL); // Pause before next pass through loop
   }
+  }  
+  if (buttonStateRe == LOW) {
+    // turn LED off:
+  pixels.clear(); // Set all pixel colors to 'off'
+  }
+  
+ 
+ if (buttonStateLi == HIGH) {
+    // turn LED on:
+  for(int i=8; i>=0; i--) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+    pixels.setPixelColor(i, pixels.Color(255, 80, 0));
+    pixels.show();   // Send the updated pixel colors to the hardware.
+    delay(DELAYVAL); // Pause before next pass through loop
+  } 
+ }
+  if (buttonStateLi == LOW) {
+    // turn LED off:
+  pixels.clear(); // Set all pixel colors to 'off'
+  }
+ }
 
 
-}
+
+  
